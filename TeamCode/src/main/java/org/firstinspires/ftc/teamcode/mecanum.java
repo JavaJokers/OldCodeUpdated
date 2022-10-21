f1d11b7e2cc03f7583c6026853da46bded7b1936
Sam948-byte <techguy763@gmail.com>
2022-10-21 11:56:18 -0500

    shit works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum.java
@@ -5,22 +5,40 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class mecanum extends LinearOpMode {
-    @Override
+    double grabberPos = 0;
+    int IMUResets = 0;
+
     public void runOpMode() throws InterruptedException {
+
+
+
+
+
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("front_left");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("back_left");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("front_right");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("back_right");
+        DcMotor arm1 = hardwareMap.dcMotor.get("arm1");
+        DcMotor duckies = hardwareMap.dcMotor.get("duckies");
+        Servo grabber = hardwareMap.servo.get("grabber");
 
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
-        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //set brake mode
+        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Retrieve the IMU from the hardware map
         BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
@@ -54,10 +72,67 @@ public class mecanum extends LinearOpMode {
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
 
-            motorFrontLeft.setPower(frontLeftPower);
-            motorBackLeft.setPower(backLeftPower);
-            motorFrontRight.setPower(frontRightPower);
-            motorBackRight.setPower(backRightPower);
+            if (gamepad1.right_bumper) {
+                motorFrontLeft.setPower(frontLeftPower * 0.25);
+                motorFrontRight.setPower(frontRightPower * 0.25);
+                motorBackLeft.setPower(backLeftPower * 0.25);
+                motorBackRight.setPower(backRightPower * 0.25);
+
+            } else if (gamepad1.left_bumper) {
+                motorFrontLeft.setPower(frontLeftPower * 0.25);
+                motorFrontRight.setPower(frontRightPower * 0.25);
+                motorBackLeft.setPower(backLeftPower * 0.25);
+                motorBackRight.setPower(backRightPower * 0.25);
+
+            } else {
+                motorFrontLeft.setPower(frontLeftPower);
+                motorFrontRight.setPower(frontRightPower);
+                motorBackLeft.setPower(backLeftPower);
+                motorBackRight.setPower(backRightPower);
+
+            }
+
+
+            if(gamepad1.dpad_left){
+                grabberPos -= 0.01;
+            }else if(gamepad1.dpad_right){
+                grabberPos += 0.01;
+            }
+
+            if(grabberPos > 1){
+                grabberPos = 1;
+            } else if(grabberPos < 0){
+                grabberPos = 0;
+            }
+
+            grabber.setPosition(grabberPos);
+
+            if(gamepad1.right_trigger == 1 && gamepad1.left_trigger == 1){
+                imu.initialize(parameters);
+                IMUResets++;
+            }
+
+            if(gamepad1.dpad_up){
+                arm1.setPower(0.5);
+            } else if(gamepad1.dpad_down){
+                arm1.setPower(-0.1);
+            }else{
+                arm1.setPower(0.1);
+            }
+
+
+            if (gamepad1.x) {
+                duckies.setPower(-0.8);
+            } else {
+                duckies.setPower(0);
+            }
+
+            // Show the wheel power.
+            telemetry.addData("IMU Resets", IMUResets);
+            telemetry.addData("Grabber", grabberPos);
+            telemetry.addData("Motors", "carousel");
+            telemetry.update();
+
         }
     }
 }
